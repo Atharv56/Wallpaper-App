2640b69bdc6be263d36141fcfad81698452bf687
Ashna-Choudhary <83903152+Ashna-Choudhary@users.noreply.github.com>
2021-11-14 20:51:05 +0530

    login page with firebase

--- lib/login.dart
+++ lib/login.dart
@@ -1,172 +1,165 @@
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-import 'main.dart';
-import 'register.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:velocity_x/velocity_x.dart';
 import 'routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:untitled1/main.dart';
+import 'package:untitled1/mainscreen.dart';
+import 'package:untitled1/progressDialog.dart';
+import 'package:untitled1/registrationScreen.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  static const String idScreen = "login";
+  TextEditingController emailTextEditingConntroller = TextEditingController();
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String? name = "";
-  bool changeButton = false;
-  final _formKey = GlobalKey<FormState>();
-
-  moveToHome(BuildContext context) async {
-    if (_formKey.currentState!.validate()) {
-      setState(() {
-        changeButton = true;
-      });
-      await Future.delayed(const Duration(seconds: 1));
-      await Navigator.pushNamed(context, MyRoutes.homeRoute);
-      setState(() {
-        changeButton = false;
-      });
-    }
-  }
-
+  TextEditingController passwordTextEditingConntroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScopeNode currentFocus = FocusScope.of(context);
-        if (!currentFocus.hasPrimaryFocus) {
-          currentFocus.unfocus();
-        }
-      },
-      child: Material(
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const SizedBox(height: 15.0),
-                Image.asset(
-                  "assets/images/hey.png",
-                  fit: BoxFit.cover,
-                  //height: 1000,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text("Welcome $name",
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: "Enter Username",
-                          labelText: "Username",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Enter a valid input";
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          name = value;
-                          setState(() {});
-                        },
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 35.0,),
+              Image(
+                image: AssetImage("images/3115.png"),
+                width: 390.0,
+                height: 250.0,
+                alignment: Alignment.center,
+              ),
+              SizedBox(height: 1.0,),
+              Text(
+                "Help App",
+                style: TextStyle(fontSize: 30.0, fontFamily: "Brand Bold", color: Colors.blue),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 1.0,),
+              Text(
+                "Login as a service receiver",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 1.0,),
+                  TextField(
+                    controller: emailTextEditingConntroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                        fontSize: 14.0,
                       ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          hintText: "Enter Password",
-                          labelText: "Password",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Enter a valid input";
-                          } else if (value.length < 6) {
-                            return "Password length must be at least 6 characters";
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      Material(
-                        color: Colors.deepPurple,
-                        borderRadius:
-                            BorderRadius.circular(changeButton ? 20 : 10),
-                        child: InkWell(
-                          onTap: () async {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const WallpaperPage(),
-                              ),
-                            );
-                          },
-                          child: AnimatedContainer(
-                            duration: const Duration(seconds: 1),
-                            width: changeButton ? 42 : 110,
-                            height: 40,
-                            alignment: Alignment.center,
-                            child: changeButton
-                                ? const Icon(Icons.done, color: Colors.white)
-                                : const Text(
-                                    "Login",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: "If you don't have an account, click on ",
-                    style: GoogleFonts.lato(
-                      fontSize: 15.0,
-                      color: context.accentColor,
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0
+                      )
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Register",
-                        style: GoogleFonts.lato(
-                          decoration: TextDecoration.underline,
-                          color: Colors.indigo,
-                          fontSize: 15.0,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () async {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const RegPage(),
-                              ),
-                            );
-                          },
-                      ),
-                    ],
                   ),
-                ),
-              ],
-            ),
+                  SizedBox(height: 1.0,),
+                  TextField(
+                    controller: passwordTextEditingConntroller,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10.0
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 1.0,),
+                  TextButton(
+                    child: Text('Login', style: TextStyle(fontSize: 20.0),),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.black,
+                      shadowColor: Colors.white,
+                      elevation: 5,
+                    ),
+                    onPressed: () {
+                      if (!emailTextEditingConntroller.text.contains("@")) {
+                        displayToastMessage("Invalid email address", context);
+                      }
+                      else if (passwordTextEditingConntroller.text.isEmpty) {
+                        displayToastMessage(
+                            "Password is mandatory",
+                            context);
+                      }
+                      else{loginAndAuthenticateUser(context);}
+
+
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Do not have an account? Register Now', style: TextStyle(fontSize: 17.0, fontFamily: "Brant Bold"),),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      backgroundColor: Colors.white,
+                      shadowColor: Colors.blue,
+                      elevation: 5,
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(context, registrationScreen.idScreen, (route) => false);
+                    },
+                  ),
+                ],
+              ),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  void loginAndAuthenticateUser(BuildContext context) async
+  {
+    final User firebaseUser = (await _firebaseAuth
+        .signInWithEmailAndPassword(email: emailTextEditingConntroller.text,
+        password: passwordTextEditingConntroller.text).catchError((errMsg){
+          Navigator.pop(context);
+          displayToastMessage("Error:" +errMsg.toString(), context);
+    })).user;
+
+    if (firebaseUser != null) //user created
+        {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context)
+            {return ProgressDialog(message: "Authenticating",);}
+          );
+
+
+      usersRef.child(firebaseUser.uid).once().then((DataSnapshot snap){
+        if(snap.value != null)
+          {
+            Navigator.pushNamedAndRemoveUntil(context, MainScreen.idScreen, (route) => false);
+            displayToastMessage("Sign In successful", context);
+          }
+        else
+          {Navigator.pop(context);
+            _firebaseAuth.signOut();
+            displayToastMessage("No records exist for this user", context);
+          }
+      });
+
+    }
+    else {
+      Navigator.pop(context);
+      displayToastMessage("Error Occured", context);
+
+    }
+
+  }
+  displayToastMessage(String message, BuildContext context){
+    Fluttertoast.showToast(msg: message);
   }
 }
